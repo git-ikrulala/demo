8f5974a2f1effd7d6a80082745aeebbfbf8cc786
ikrulala <ikrulala@ikrulalademacbook-pro.local>
2017-06-11 11:09:22 +0800

    test 1

--- demo/ViewController.swift
+++ demo/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = UIColor.yellow
         print("hahahah")
+        print("hahahah")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
